5c8e512cab0710901c7e96e8eba5caa1a052e2e5
anbuter <antebuterintubi@hotmail.com>
2019-01-02 15:11:05 +0100

    Added possibility of moving by multiple field at once.

--- Snake/Snake/Controllers/SnakeController.cs
+++ Snake/Snake/Controllers/SnakeController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 using SnakeGame.Entities;
 using SnakeGame.Utils;
+using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
 
 namespace SnakeGame.Controllers
 {
@@ -36,6 +38,47 @@ namespace SnakeGame.Controllers
                     return;
                 }
                 instance.snake.BaseVelocity = keyMapping [e.KeyCode];
+                HandleMultipleMovement();
+            }
+        }
+
+        private static void HandleMultipleMovement ()
+        {
+            if (Keyboard.IsKeyDown(Key.D1))
+            {
+                instance.snake.MoveByAmmount(1);
+            }
+            else if (Keyboard.IsKeyDown(Key.D2))
+            {
+                instance.snake.MoveByAmmount(2);
+            }
+            else if (Keyboard.IsKeyDown(Key.D3))
+            {
+                instance.snake.MoveByAmmount(3);
+            }
+            else if (Keyboard.IsKeyDown(Key.D4))
+            {
+                instance.snake.MoveByAmmount(4);
+            }
+            else if (Keyboard.IsKeyDown(Key.D5))
+            {
+                instance.snake.MoveByAmmount(5);
+            }
+            else if (Keyboard.IsKeyDown(Key.D6))
+            {
+                instance.snake.MoveByAmmount(6);
+            }
+            else if (Keyboard.IsKeyDown(Key.D7))
+            {
+                instance.snake.MoveByAmmount(7);
+            }
+            else if (Keyboard.IsKeyDown(Key.D8))
+            {
+                instance.snake.MoveByAmmount(8);
+            }
+            else if (Keyboard.IsKeyDown(Key.D9))
+            {
+                instance.snake.MoveByAmmount(9);
             }
         }
     }
