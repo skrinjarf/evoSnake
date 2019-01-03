595184a51a831bc80ec625c138a12c59a7033080
anbuter <antebuterintubi@hotmail.com>
2019-01-04 00:42:26 +0100

    Fixed bug with items spawning in walls again

--- Snake/Snake/Items/Item.cs
+++ Snake/Snake/Items/Item.cs
@@ -28,7 +28,7 @@ namespace SnakeGame.Items
             Xpos = rnd.Next(0, WorldRenderer.instance.World.Dimensions.X);
             Ypos = rnd.Next(0, WorldRenderer.instance.World.Dimensions.Y);
             Vector2 pos = new Vector2(Xpos, Ypos);
-            while (Wall.AnyWall(pos) && FindItem(pos) != null)
+            while (Wall.AnyWall(pos) || FindItem(pos) != null)
             {
                 Xpos = rnd.Next(0, WorldRenderer.instance.World.Dimensions.X);
                 Ypos = rnd.Next(0, WorldRenderer.instance.World.Dimensions.Y);
